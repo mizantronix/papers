5731f350ad325dc3c5bfc2ec43508b7c7606b8a1
a.guliganov <mizantronix@gmail.com>
2022-02-15 23:45:55 +0700

    auth

--- papers-server/Api/Attributes/AccessAttribute.cs
+++ papers-server/Api/Attributes/AccessAttribute.cs
@@ -1,23 +1,50 @@
-﻿namespace Papers.Api.Attributes
+﻿using Papers.Data.MsSql.Models;
+
+namespace Papers.Api.Attributes
 {
     using System;
 
-    using Microsoft.AspNetCore.Authorization;
-
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
     using Papers.Common.Enums;
 
+    // TODO async
     [AttributeUsage(AttributeTargets.Method)]
-    public class AccessAttribute : AuthorizeAttribute
+    public class AccessAttribute : Attribute, IAuthorizationFilter
     {
-        public UserState UserState;
+        private const string HeaderName = "Authorization";
+
+        public UserState? UserState;
 
         private AccessAttribute()
         {
         }
 
-        public AccessAttribute(UserState userState)
+        // TODO use something else instead UserState
+        public AccessAttribute(UserState? userState)
         {
             this.UserState = userState;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (context != null)
+            {
+                var authCode = context.HttpContext.Request.Headers[HeaderName];
+                
+                if (this.UserState.HasValue && this.UserState != Common.Enums.UserState.New && string.IsNullOrEmpty(authCode))
+                {
+                    context.Result = new UnauthorizedObjectResult($"{HeaderName} is required");
+                    return;
+                }
+
+                // TODO token validation
+                if (false)
+                {
+                    context.Result = new UnauthorizedObjectResult($"{HeaderName} is invalid");
+                    return;
+                }
+            }
         }
     }
 }
